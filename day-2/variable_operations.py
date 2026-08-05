a9735e7ecc2ac3b7b4cfa5a6ea5d4966a0540317
Eswar Nandan Perabattula <eswarnandan903@gmail.com>
2026-08-05 17:47:50 +0530

    Python intro

---
+++ day-2/variable_operations.py
@@ -0,0 +1,25 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+================================================= RESTART: C:/Users/eswar/OneDrive/Desktop/Python-course-work/day - Copy (2)/keyword.py ================================================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+>>> a=b=c=10
+>>> a
+10
+>>> b
+10
+>>> c
+10
+>>> a,b,c=10,20,30
+>>> a
+10
+>>> d
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    d
+NameError: name 'd' is not defined. Did you mean: 'id'?
+>>> b
+20
+>>> c
+30
